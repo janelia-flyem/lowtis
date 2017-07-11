363c72e159d4213b8597caf7bd2c299a48a1e000
Stuart Berg <bergs@janelia.hhmi.org>
2017-07-11 16:21:19 -0400

    conda-recipe: On Mac, use clang.  Also, upgrade to boost 1.63.0

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,8 +1,12 @@
 # Depending on our platform, shared libraries end with either .so or .dylib
-if [[ `uname` == 'Darwin' ]]; then
+if [[ $(uname) == 'Darwin' ]]; then
     DYLIB_EXT=dylib
+    CC=clang
+    CXX=clang++
 else
     DYLIB_EXT=so
+    CC=gcc
+    CXX=g++
 fi
 
 CONFIGURE_ONLY=0
@@ -19,13 +23,14 @@ fi
 mkdir -p build # Using -p here is convenient for calling this script outside of conda.
 cd build
 cmake ..\
-        -DCMAKE_C_COMPILER="${PREFIX}/bin/gcc" \
-        -DCMAKE_CXX_COMPILER="${PREFIX}/bin/g++" \
+        -DCMAKE_C_COMPILER=${CC} \
+        -DCMAKE_CXX_COMPILER=${CXX} \
         -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
         -DCMAKE_PREFIX_PATH="${PREFIX}" \
         -DCMAKE_CXX_FLAGS=-I"${PREFIX}/include" \
         -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
         -DCMAKE_EXE_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
+        -DBOOST_ROOT=${PREFIX} \
         -DBoost_LIBRARY_DIR="${PREFIX}/lib" \
         -DBoost_INCLUDE_DIR="${PREFIX}/include" \
         -DLIBDVID_INCLUDE="${PREFIX}/include" \
